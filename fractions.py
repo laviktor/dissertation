610ad7a891945b600c2a7d81de24d27871f80423
V. Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-18 00:52:20 -0400

    Update fractions.py

--- fractions.py
+++ fractions.py
@@ -1,1 +1,4 @@
-print('Calculate the following fractions: 3/5 + 
+print('Calculate the following fractions: 1/2 + 5/6. Simplify your answer as necessary.')
+answer = input('Enter your answer: ')
+if answer == '4/3':
+  print('Correct!')
